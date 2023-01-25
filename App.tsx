7826eb50e871839970ffa6bbe918682738bd2e87
Eduarda Carvalho <eduardacarvalho9833@gmail.com>
2023-01-25 09:49:24 -0300

    style: add expo font

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
+/* eslint-disable camelcase */
 import { Groups } from '@screens/Groups';
 import { ThemeProvider } from 'styled-components';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { ActivityIndicator } from 'react-native';
 import theme from './src/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+     { fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
-    
   );
 }
