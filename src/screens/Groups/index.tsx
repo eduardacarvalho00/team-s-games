5032266ecf47831f0b1c5a2e39fe4f875e007267
Eduarda Carvalho <eduardacarvalho9833@gmail.com>
2023-01-26 13:39:44 -0300

    feat: list groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,30 @@
+/* eslint-disable no-unused-vars */
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Hightlight } from '@components/HightLight';
+import { ListEmpty } from '@components/ListEmpty';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 import { Container } from './styles';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Galera da Dungeons', 'Amigos', ' Familia']);
+
   return (
     <Container>
       <Header showBackButton={false} />
       <Hightlight title="Turmas" subtitle="Jogue com a sua turma" />
-      <GroupCard title="Galera do ignite" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <GroupCard title={item} />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={<ListEmpty message="Que tal cadastrar a primeira turma?" />}
+      />
+     
     </Container>
   );
 }
