cdd163ef01507a4405ee559a7596f02ba5ecf9f4
Eduarda Carvalho <eduardacarvalho9833@gmail.com>
2023-01-30 10:15:00 -0300

    feat: created component input

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@components/Button';
 import { Header } from '@components/Header';
 import { Hightlight } from '@components/HightLight';
+import { Input } from '@components/Input';
 import { Container, Content, Icon } from './styles';
 
 export function NewGroup() {
@@ -10,8 +11,11 @@ export function NewGroup() {
 
       <Content>
         <Icon />
-        <Hightlight title="Nova turma" subtitle="crie a turma paa adicionar as pessoas" />
-        <Button title="criar" />
+        <Hightlight title="Nova turma" subtitle="crie a turma para adicionar as pessoas" />
+        <Input 
+          placeholder="Nome da turma"
+        />
+        <Button title="criar" style={{ marginTop: 20 }} />
       </Content>
       
     </Container>
