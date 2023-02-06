5d1403768e01f2f66d53520fca0abb051bf0d433
Eduarda Carvalho <eduardacarvalho9833@gmail.com>
2023-02-06 11:09:00 -0300

    feat: listing players

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -5,7 +5,7 @@ interface Props extends TouchableOpacityProps, FilterStyleProps{
  title: string
 }
 
-export function Filter({ title, isActive = true, ...rest }: Props) {
+export function Filter({ title, isActive = false, ...rest }: Props) {
   return (
     <Container isActive={isActive} {...rest}>
       <Title>{title}</Title>
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-bind */
 /* eslint-disable no-unused-vars */
 import { Button } from '@components/Button';
 import { ButtonIcon } from '@components/ButtonIcon';
@@ -8,8 +9,11 @@ import { Input } from '@components/Input';
 import { ListEmpty } from '@components/ListEmpty';
 import { PlayerCard } from '@components/PlayerCard';
 import { useRoute } from '@react-navigation/native';
+import { playerAddByGroup } from '@storage/player/playerAddByGroup';
+import { playersGetByGroup } from '@storage/player/playersGetByGroup';
+import { AppError } from '@utils/AppError';
 import { useState } from 'react';
-import { FlatList } from 'react-native';
+import { Alert, FlatList } from 'react-native';
 import {
   Container, Form, HeaderList, NumbersOfPlayers, 
 } from './styles';
@@ -19,11 +23,35 @@ interface RouteParams{
 }
 
 export function Players() {
+  const [newPlayerName, setNewPlayerName] = useState('');
   const [team, setTeam] = useState('Time A');
   const [players, setPlayers] = useState(['Duda', 'Mirela']);
 
   const route = useRoute();
   const { group } = route.params as RouteParams;
+
+  async function handleAddPlayer() {
+    if (newPlayerName.trim().length === 0) {
+      return Alert.alert('Nova Pessoa', 'Informe o nome do jogador para adicionar.');
+    }
+    const newPlayer = {
+      name: newPlayerName,
+      team,
+    };
+
+    try {
+      await playerAddByGroup(newPlayer, group);
+      const player = await playersGetByGroup(group);
+      console.log(player);
+    } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert('Nova pessoa', error.message);
+      } else {
+        Alert.alert('Nova pessoa', 'Não foi possivel adicionar');
+      }
+    }
+    return null;
+  }
 
   return (
     <Container>
@@ -34,12 +62,13 @@ export function Players() {
       />
 
       <Form> 
-        <Input placeholder="Nome da pessoa" autoCorrect={false} />
-        <ButtonIcon nameIcon="add" />
+        <Input placeholder="Nome da pessoa" autoCorrect={false} onChangeText={setNewPlayerName} />
+        <ButtonIcon nameIcon="add" onPress={handleAddPlayer} />
       </Form>
+
       <HeaderList>
         <FlatList 
-          data={['time a', 'time b']}
+          data={['TIME A', 'TIME B']}
           keyExtractor={(item) => item}
           renderItem={({ item }) => (
             <Filter
